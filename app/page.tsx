4acaccfce348cad114ed240b7b7ca59aae390bdb
ArnabSaga <arnabdey15091@gmail.com>
2024-11-17 21:05:40 +0600

    new functionality added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import PatientForm from "@/components/form/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home(){
-    return(
+export default function Home({ searchParams }: SearchParamProps) {
+    const isAdmin = searchParams.admin === 'true';
+
+    return (
         <div className="flex h-screen max-h-screen">
-            {/* TODO:  OTP VERIFICATION*/}
+            { isAdmin && <PasskeyModal/>}
 
             <section className="remove-scrollbar container my-auto">
                 <div className="sub-container max-w-[496px]">
-                    <Image 
+                    <Image
                         src="/assets/icons/logo-full.svg"
                         width={1000}
                         height={1000}
@@ -21,14 +24,14 @@ export default function Home(){
 
                     <div className="text-14-regular mt-8 flex justify-between">
                         <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
-                        <Link href="/?admin=true"className="text-green-500">
+                        <Link href="/?admin=true" className="text-green-500">
                             Admin
                         </Link>
                     </div>
                 </div>
             </section>
 
-            <Image 
+            <Image
                 src="/assets/images/onboarding-img.png"
                 height={1000}
                 width={1000}
--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -4,7 +4,7 @@ import { ID } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, BUCKET_ID, DATABASE_ID, databases, ENDPOINT } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-export const createAppointment  = async (appointment: CreateAppointmentParams) => {
+export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
         const newAppointment = await databases.createDocument(
             DATABASE_ID!,
@@ -16,6 +16,20 @@ export const createAppointment  = async (appointment: CreateAppointmentParams) =
         return parseStringify(newAppointment);
     } catch (error) {
         console.log("Create appointment error:", error);
-        
+
     }
-}
+};
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId
+        );
+
+        return parseStringify(appointment);
+    } catch (error) {
+        console.log("Get appointment error:", error);
+    }
+};
